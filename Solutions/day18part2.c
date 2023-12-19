70abf1f61d5a14c22b0cd54605ad9456e164ea5e
Paige <paige.ucf.edu>
2023-12-18 23:43:24 -0500

    forgot to delete and rename a file

--- Solutions/day18part2.c
+++ Solutions/day18part2.c
@@ -4,39 +4,18 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define FILE_NAME "test.txt"
-#define NUM_INSTR 14
-#define MAX_EDGES 2048
+#define FILE_NAME "input.txt"
+#define NUM_INSTR 674
+
+typedef struct {
+    long x;
+    long y;
+} Vertex;
 
 typedef struct {
     char dir;
     int length;
 } Instr;
-
-typedef struct {
-    long c;
-    long r1;
-    long r2;
-} VerticalEdge;
-
-int compareEdges(const void* a, const void* b) {
-    VerticalEdge* e1 = (VerticalEdge*) a;
-    VerticalEdge* e2 = (VerticalEdge*) b;
-
-    return e1->c - e2->c;
-}
-
-int compareLongs(const void* a, const void* b) {
-    long* l1 = (long*) a;
-    long* l2 = (long*) b;
-    if (*l1 - *l2 == 0l) {
-        return 0;
-    }
-    if (*l1 - *l2 < 0l) {
-        return -1;
-    }
-    return 1;
-}
 
 long max(long a, long b) {
     if (a > b) {
@@ -67,20 +46,46 @@ int main() {
 
         fscanf(f, "%c %d (#%5x%d) ", &fakeDir, &fakeLength, &plan[i].length, &dirInd);
 
-        plan[i].length = fakeLength;
-        plan[i].dir = fakeDir;
+        // plan[i].length = fakeLength;
+        // plan[i].dir = fakeDir;
 
         // yes, you can index a string literal in C for some reason
-        // plan[i].dir = "RDLU"[dirInd];
+        plan[i].dir = "RDLU"[dirInd];
 
         printf("%d: %c %d\n", i, plan[i].dir, plan[i].length);
     }
 
-    int numEdges = 0;
-    VerticalEdge edges[MAX_EDGES];
+    // store the vertices of the polygons
+    Vertex verts[NUM_INSTR + 1];
+    verts[0] = (Vertex){0,0};
+    int numVertices = 1;
+
+    int offsetX = 0;
+    int offsetY = 0;
+
+    char clockwiseMap[256];
+    char counterClockwiseMap[256];
+    clockwiseMap['U'] = 'R';
+    clockwiseMap['R'] = 'D';
+    clockwiseMap['D'] = 'L';
+    clockwiseMap['L'] = 'U';
+
+    int isClockWise = 0;
+
+    // this code only works if the first instruction is R and it is going clockwise, which is the case for my input and the test case
+    if (clockwiseMap[plan[NUM_INSTR-1].dir] == plan[0].dir) {
+        printf("Clockwise!\n");
+        isClockWise = 1;
+    }
+    else {
+        printf("Counter clockwise!\n");
+        return 1;
+    }
 
     for (int i = 0; i < NUM_INSTR; i++) {
         char dir = plan[i].dir;
+        
+        char nextDir = plan[(i+1) % NUM_INSTR].dir;
         int length = plan[i].length;
 
         // change in x and y for the direction
@@ -89,81 +94,61 @@ int main() {
 
         switch (dir) {
             case 'U':
-                dy = -length;
+                dy -= length;
+                if (nextDir == clockwiseMap['U']) {
+                    offsetY = 0;
+                }
+                else {
+                    offsetY = 1;
+                }
                 break;
             case 'D':
-                dy = length;
+                dy += length;
+                if (nextDir == clockwiseMap['D']) {
+                    offsetY = 1;
+                }
+                else {
+                    offsetY = 0;
+                }
                 break;
             case 'L':
-                dx = -length;
+                dx -= length;
+                if (nextDir == clockwiseMap['L']) {
+                    offsetX = 0;
+                }
+                else {
+                    offsetX = 1;
+                }
                 break;
             case 'R':
-                dx = length;
+                dx += length;
+                if (nextDir == clockwiseMap['R']) {
+                    offsetX = 1;
+                }
+                else {
+                    offsetX = 0;
+                }
                 break;
         }
         long startRow = curRow;
 
         curRow += dy;
         curCol += dx;
+        verts[numVertices] = (Vertex){curCol + offsetX, curRow + offsetY};
 
-        if (dir == 'U' || dir == 'D') {
-            edges[numEdges] = (VerticalEdge){curCol, min(startRow, curRow), max(startRow, curRow)};
-            numEdges ++;
-            assert(numEdges < MAX_EDGES);
-        }
+        numVertices ++;
     }
 
-    // optimized raycasting algorithm
-
-    // rows where a vertical edge starts
-    long sigRows[MAX_EDGES * 2];
-    int numSigRows = 0;
-
-    for (int i = 0; i < numEdges; i++) {
-        sigRows[numSigRows] = edges[i].r1;
-        numSigRows ++;
-        assert(numSigRows < MAX_EDGES * 2);
-        sigRows[numSigRows] = edges[i].r2;
-        numSigRows ++;
-        assert(numSigRows < MAX_EDGES * 2);
-    }
-
-    // sort the edges by their columns and the significant rows
-    qsort(edges, numEdges, sizeof(VerticalEdge), compareEdges);
-    qsort(sigRows, numSigRows, sizeof(long), compareLongs);
-
+    // Algorithm from: https://stackoverflow.com/questions/451426/how-do-i-calculate-the-area-of-a-2d-polygon
     long totalArea = 0;
 
-    // calculate the area of the non significant rows
-    for (int i = 0; i < numSigRows; i++) {
-        long r = sigRows[i];
-        long area = 0;
-        int inShape = 0;
-        long prevCol = 0;
-
-        for (int j = 0; j < numEdges; j++) {
-            if (r >= edges[j].r1 && r < edges[j].r2) {
-                if (!inShape) {
-                    inShape = 1;
-                    prevCol = edges[j].c;
-                }
-                else {
-                    inShape = 0;
-                    area += edges[j].c - prevCol + 1;
-                }
-            }
-        }
-        assert(area >= 0);
-        if (i != numSigRows - 1) {
-            if (sigRows[i+1] != sigRows[i]) {
-                printf("%ld, %ld\n", area, (sigRows[i+1] - sigRows[i] - 1));
-                area *= (sigRows[i+1] - sigRows[i] - 1);
-                totalArea += area;
-            }
-        }
-        assert(totalArea >= 0);
+    for (int i = 0; i < numVertices; i++) {
+        int j = (i+1) % numVertices;
+        totalArea += verts[i].x * verts[j].y;
+        totalArea -= verts[i].y * verts[j].x;
     }
 
+    totalArea /= 2;
 
     printf("%ld\n", totalArea);
 
